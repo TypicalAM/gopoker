8ef94dbf575cfe8d63e32773443325476016e123
TypicalAM <tpam_piaseczny@protonmail.com>
2023-04-10 16:19:22 +0200

    feat(front): added the logout page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Logout from './pages/Logout';
 
 const router = createBrowserRouter([
 	{
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
 	{
 		path: '/login',
 		element: <Login />,
+	},
+	{
+		path: '/logout',
+		element: <Logout />,
 	}
 ]);
 
